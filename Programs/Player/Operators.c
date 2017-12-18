dd6cf2d24e2da4e120e5297b8c7b4e51db4c1e03
muruga26 <33129463+muruga26@users.noreply.github.com>
2017-12-18 22:09:27 +0530

    Create Operators.c

---
+++ Programs/Player/Operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int a=5,b=5,add,sub,mul,mod,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+printf("%d,%d,%d,%d,%d",add,sub,mul,div,mod);
+return 0;
+}
