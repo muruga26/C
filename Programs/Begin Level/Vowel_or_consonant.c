0767c4e2fe1ca44574363d464358f981a4c8bef0
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-02-20 21:34:07 +0530

    Update Vowel_or_consonant.c

--- Programs/Begin Level/Vowel_or_consonant.c
+++ Programs/Begin Level/Vowel_or_consonant.c
@@ -6,9 +6,9 @@ int main(void)
   scanf("%c", &ch);
   if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
     if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch== 'u' || ch=='U')
-      printf("%c is a vowel.\n", ch);
+      printf("Vowel");
     else
-      printf("%c is a consonant.\n", ch);
+      printf("Consonant");
   }
   else
     printf("%c is neither a vowel nor a consonant.\n", ch);
