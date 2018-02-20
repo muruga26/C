4d5348cf75233cb227ee0fc4d9d5752a790ec30a
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-02-20 20:17:25 +0530

    Update Greatest_among_three_numbers.c

--- Programs/Begin Level/Greatest_among_three_numbers.c
+++ Programs/Begin Level/Greatest_among_three_numbers.c
@@ -5,15 +5,15 @@ int main(void) {
 	scanf("%d %d %d",&a,&b,&c);
 	if(a>b&&a>c)
 	{
-	printf("Greatest among three is:%d",a);
+	printf("%d",a);
 	}
 	else if(b>a&&b>c)
 	{
-	printf("Greatest among three is:%d",b);
+	printf("%d",b);
 	}
 	else
 	{
-	printf("Greatest among three is:%d",c);
+	printf("%d",c);
 	}
 	return 0;
 }
