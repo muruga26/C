4bc6c9d47e6b3df944458f77582bec64c5245de3
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-02-20 21:54:48 +0530

    Create Leap_year.c

---
+++ Programs/Begin Level/Leap_year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int year;
+  scanf("%d", &year);
+ 
+  if ( year%400 == 0) 
+    printf("Yes");
+  else if ( year%100 == 0)
+    printf("No");
+  else if ( year%4 == 0 ) 
+    printf("Yes");
+  else 
+    printf("No");  
+    
+return 0;
+}
