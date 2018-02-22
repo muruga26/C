17f143a99742ee8438d91b15405b0d21801626f1
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-02-22 15:12:36 +0530

    Create Word_count.java

---
+++ Programs/Begin Level/set_7/Word_count.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String a;
+		int n=0;
+		a=sc.nextLine();
+		String b[]=a.split(" ");
+		System.out.println(b.length);
+		}
+}
