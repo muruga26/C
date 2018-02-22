68c48b55f97de9947edcf1ad0979f16360f23fa0
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-02-22 15:06:33 +0530

    Create print_digits.java

---
+++ Programs/Begin Level/set_6/print_digits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.next();
+ 
+		for(int i=0;i<a.length();i++)
+		{
+			System.out.print(a.charAt(i)+" ");
+		}
+	}
+}
