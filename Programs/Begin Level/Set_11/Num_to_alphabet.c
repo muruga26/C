a45a6438a82b3c8be2cdea1ff5f6082ce99719c3
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-03-04 22:08:56 +0530

    Create Num_to_alphabet.c

---
+++ Programs/Begin Level/Set_11/Num_to_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	
+	char a[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int n;
+	scanf("%d",&n);
+	printf("%c",a[n-1]);
+	return 0;
+}
