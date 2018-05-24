5c241543a4f1cd1757a154a6257877c06f3ccf65
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-05-24 22:41:19 +0530

    Create array_a[i]+a[j]=a[k].c

---
+++ Programs/Hunter/set_1/array_a[i]+a[j]=a[k].c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	
+	int n,a[20],i,j,k;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			for(k=j+1;k<n;k++)
+			{
+				if(a[i]+a[j]==a[k])
+				{
+					printf("%d %d %d",a[i],a[j],a[k]);
+					printf("\n");
+					
+				}
+			}
+		}
+	}
+	return 0;
+}
