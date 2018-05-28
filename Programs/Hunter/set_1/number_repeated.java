0ca3855f5b5e67e4f1039d3b046cb7ea16e4861e
muruga26 <33129463+muruga26@users.noreply.github.com>
2018-05-28 18:05:40 +0530

    Create number_repeated.java

---
+++ Programs/Hunter/set_1/number_repeated.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Unique
+{
+	public static void main(String[] args)
+	{
+		int n,i,j,x=0,count=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+			    count++;
+                x=a[i];
+                break;
+			}
+		}break;
+		}
+		if(count==0)
+		{
+		    System.out.println("Unique");
+		}
+	    else
+		{
+	        System.out.println(x);
+		}
+}	
+}
